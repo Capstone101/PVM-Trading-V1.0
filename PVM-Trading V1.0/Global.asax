fa6d949cf5825a73cd17564ec53500a89060f29e
amargota <33738610+amargota@users.noreply.github.com>
2018-01-09 20:57:29 +0800

    Add project files.

---
+++ PVM-Trading V1.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PVM_Trading_V1._0.MvcApplication" Language="C#" %>
---
+++ PVM-Trading V1.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PVM_Trading_V1._0.Startup))]
+namespace PVM_Trading_V1._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
